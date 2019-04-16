7a64fc80754d66e4f407f3517e6d500e43958b2e
dronstudy_shubham <shubhammehta.dronstudy@gmail.com>
2019-04-16 20:04:41 +0530

    optionId

--- backend/models/question.js
+++ backend/models/question.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 const questionSchema = mongoose.Schema({
   question: { type: String, required: true },
  // creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+  optionId: { type: mongoose.Schema.Types.ObjectId, ref: "Options", required: true },
 });
 module.exports = mongoose.model("Questions", questionSchema);
